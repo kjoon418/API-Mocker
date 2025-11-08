59be26a79cff5a078721bd0d4075bd883a2863c1
Kim, Joon <kjoon418@naver.com>
2025-11-08 17:06:59 +0900

    feat(server): `Response` DTO의 내용을 통해 HTTP 응답을 작성하는 기능 구현

---
+++ src/main/kotlin/framework/response/HttpResponseWriter.kt
@@ -0,0 +1,18 @@
+package framework.response
+
+import framework.config.CHARACTER_ENCODING
+import framework.dto.Response
+import jakarta.servlet.http.HttpServletResponse
+
+class HttpResponseWriter {
+    fun write(httpResponse: HttpServletResponse, responseData: Response) {
+        httpResponse.status = responseData.code
+        httpResponse.contentType = responseData.contentType
+        httpResponse.characterEncoding = CHARACTER_ENCODING
+
+        httpResponse.writer.use { writer ->
+            writer.write(responseData.body)
+            writer.flush()
+        }
+    }
+}
