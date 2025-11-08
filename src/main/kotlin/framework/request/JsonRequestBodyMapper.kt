f9b4dbb3fffe8015d9aa1cec749007c692d0cf7e
Kim, Joon <kjoon418@naver.com>
2025-11-08 18:05:55 +0900

    fix(mapper): `Action`의 `body` 타입이 빈 값일 때에도 정상 동작하도록 로직 개선

--- src/main/kotlin/framework/request/JsonRequestBodyMapper.kt
+++ src/main/kotlin/framework/request/JsonRequestBodyMapper.kt
@@ -2,24 +2,19 @@ package framework.request
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import framework.exception.IllegalRequestException
 
 class JsonRequestBodyMapper(
     private val objectMapper: ObjectMapper = jacksonObjectMapper()
 ) : HttpBodyMapper {
     override fun map(rawBody: String, type: Class<*>): Any {
-        if (rawBody.isBlank()) {
-            return createEmptyInstance(type)
+        if (type.isEmpty()) {
+            return Unit
         }
 
         return objectMapper.readValue(rawBody, type)
     }
 
-    private fun createEmptyInstance(type: Class<*>): Any {
-        return try {
-            type.getDeclaredConstructor().apply { isAccessible = true }.newInstance()
-        } catch (e: Exception) {
-            throw IllegalRequestException("body 매핑에 실패했습니다: ${type.name}")
-        }
+    private fun Class<*>.isEmpty(): Boolean {
+        return this == Void::class.java || this == Void.TYPE || this == Unit::class.java
     }
 }
